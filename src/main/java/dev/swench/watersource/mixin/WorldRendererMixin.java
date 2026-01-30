9ef38040fd287b1e21f1f60148cb7b6f044129b8
Swench44 <sahinulkem6@gmail.com>
2026-01-30 19:54:56 +0300

    Source Code Upload

--- src/main/java/dev/swench/watersource/mixin/WorldRendererMixin.java
+++ src/main/java/dev/swench/watersource/mixin/WorldRendererMixin.java
@@ -1,17 +1,14 @@
 package dev.swench.watersource.mixin;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import dev.swench.watersource.Config;
 import dev.swench.watersource.util.RenderUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.*;
-import net.minecraft.client.gl.ShaderProgramKeys;
 import net.minecraft.client.util.ObjectAllocator;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.util.math.BlockPos;
-import org.joml.Matrix4f;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,64 +17,54 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
-
     @Inject(method = "render", at = @At("RETURN"))
-    private void onRender(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci) {
+    private void onRender(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, org.joml.Matrix4f matrix4f, org.joml.Matrix4f matrix4f2, CallbackInfo ci) {
         if (!Config.waterEnabled && !Config.lavaEnabled) return;
 
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.world == null || client.player == null) return;
 
-        Vec3d cameraPos = camera.getPos();
-        
-        RenderSystem.setShader(ShaderProgramKeys.POSITION_COLOR);
-        RenderSystem.disableDepthTest();
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.disableCull();
-
         MatrixStack matrices = new MatrixStack();
-        matrices.multiplyPositionMatrix(positionMatrix);
+        matrices.multiplyPositionMatrix(matrix4f);
+
+        Vec3d cameraPos = camera.getPos();
+        matrices.translate(-cameraPos.x, -cameraPos.y, -cameraPos.z);
+
+        VertexConsumerProvider.Immediate vertexConsumers = client.getBufferBuilders().getEntityVertexConsumers();
 
         BlockPos playerPos = client.player.getBlockPos();
+        int waterRadiusSq = Config.waterRadius * Config.waterRadius;
+        int lavaRadiusSq = Config.lavaRadius * Config.lavaRadius;
         int maxRadius = Math.max(Config.waterRadius, Config.lavaRadius);
 
-        for (int x = -maxRadius; x <= maxRadius; x++) {
-            for (int y = -maxRadius; y <= maxRadius; y++) {
-                for (int z = -maxRadius; z <= maxRadius; z++) {
-                    int distSq = x*x + y*y + z*z;
-                    BlockPos pos = playerPos.add(x, y, z);
-                    FluidState fluidState = client.world.getFluidState(pos);
+        float waterR = (Config.waterColor.getValue() >> 16 & 0xFF) / 255f;
+        float waterG = (Config.waterColor.getValue() >> 8 & 0xFF) / 255f;
+        float waterB = (Config.waterColor.getValue() & 0xFF) / 255f;
+        float waterA = (100 - Config.waterAlpha) / 100f;
 
-                    if (fluidState.isEmpty()) continue;
+        float lavaR = (Config.lavaColor.getValue() >> 16 & 0xFF) / 255f;
+        float lavaG = (Config.lavaColor.getValue() >> 8 & 0xFF) / 255f;
+        float lavaB = (Config.lavaColor.getValue() & 0xFF) / 255f;
+        float lavaA = (100 - Config.lavaAlpha) / 100f;
 
-                    if (Config.waterEnabled && distSq <= Config.waterRadius * Config.waterRadius) {
-                        if (fluidState.getFluid() == Fluids.WATER || fluidState.getFluid() == Fluids.FLOWING_WATER) {
-                            if (fluidState.isStill() && fluidState.getLevel() == 8) {
-                                matrices.push();
-                                matrices.translate(pos.getX() - cameraPos.x, pos.getY() - cameraPos.y, pos.getZ() - cameraPos.z);
-                                RenderUtils.drawBox(matrices, BlockPos.ORIGIN, Config.waterColor.getValue(), Config.waterFill, 100 - Config.waterAlpha);
-                                matrices.pop();
-                            }
-                        }
-                    }
+        for (BlockPos pos : BlockPos.iterate(playerPos.add(-maxRadius, -maxRadius, -maxRadius), playerPos.add(maxRadius, maxRadius, maxRadius))) {
+            double distSq = pos.getSquaredDistance(playerPos);
+            FluidState fluidState = client.world.getFluidState(pos);
+            if (fluidState.isEmpty()) continue;
 
-                    if (Config.lavaEnabled && distSq <= Config.lavaRadius * Config.lavaRadius) {
-                        if (fluidState.getFluid() == Fluids.LAVA || fluidState.getFluid() == Fluids.FLOWING_LAVA) {
-                            if (fluidState.isStill() && fluidState.getLevel() == 8) {
-                                matrices.push();
-                                matrices.translate(pos.getX() - cameraPos.x, pos.getY() - cameraPos.y, pos.getZ() - cameraPos.z);
-                                RenderUtils.drawBox(matrices, BlockPos.ORIGIN, Config.lavaColor.getValue(), Config.lavaFill, 100 - Config.lavaAlpha);
-                                matrices.pop();
-                            }
-                        }
-                    }
+            if (Config.waterEnabled && distSq <= waterRadiusSq) {
+                if (fluidState.isOf(Fluids.WATER) && fluidState.isStill() && fluidState.getLevel() == 8) {
+                    RenderUtils.drawBox(matrices, vertexConsumers, pos, waterR, waterG, waterB, waterA, Config.waterFill, true);
+                }
+            }
+
+            if (Config.lavaEnabled && distSq <= lavaRadiusSq) {
+                if (fluidState.isOf(Fluids.LAVA) && fluidState.isStill() && fluidState.getLevel() == 8) {
+                    RenderUtils.drawBox(matrices, vertexConsumers, pos, lavaR, lavaG, lavaB, lavaA, Config.lavaFill, true);
                 }
             }
         }
 
-        RenderSystem.enableCull();
-        RenderSystem.enableDepthTest();
-        RenderSystem.disableBlend();
+        vertexConsumers.draw();
     }
 }
--- src/main/java/dev/swench/watersource/util/RenderUtils.java
+++ src/main/java/dev/swench/watersource/util/RenderUtils.java
@@ -1,126 +1,106 @@
 package dev.swench.watersource.util;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.render.*;
-import net.minecraft.client.gl.ShaderProgramKeys;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import org.joml.Matrix4f;
 
 public class RenderUtils {
-
-    public static void drawBox(MatrixStack matrixStack, BlockPos pos, int color, boolean fill, int alphaPercent) {
-        float r = (float) (color >> 16 & 255) / 255.0F;
-        float g = (float) (color >> 8 & 255) / 255.0F;
-        float b = (float) (color & 255) / 255.0F;
-        float a = (float) alphaPercent / 100.0F;
-
-        Matrix4f matrix = matrixStack.peek().getPositionMatrix();
-        Tessellator tessellator = Tessellator.getInstance();
-        
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.setShader(ShaderProgramKeys.POSITION_COLOR);
-        RenderSystem.disableDepthTest();
-
+    public static void drawBox(MatrixStack matrices, VertexConsumerProvider vertexConsumers, BlockPos pos, float red, float green, float blue, float alpha, boolean fill, boolean outline) {
         Box box = new Box(pos);
-        
+        renderBox(matrices, vertexConsumers, box, red, green, blue, alpha, fill, outline);
+    }
+
+    public static void renderBox(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Box box, float red, float green, float blue, float alpha, boolean fill, boolean outline) {
+        Matrix4f matrix = matrices.peek().getPositionMatrix();
+
         if (fill) {
-            BufferBuilder bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
-            drawFilledBox(matrix, bufferBuilder, box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ, r, g, b, a);
-            BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+            VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getGui());
+            drawFilledBox(matrix, consumer, box, red, green, blue, alpha);
         }
 
-        BufferBuilder outlineBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
-        drawBoxOutlineAsQuads(matrix, outlineBuilder, box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ, r, g, b, a);
-        BufferRenderer.drawWithGlobalProgram(outlineBuilder.end());
-        
-        RenderSystem.enableDepthTest();
-        RenderSystem.disableBlend();
-    }
-
-    private static void drawBoxOutlineAsQuads(Matrix4f matrix, BufferBuilder buffer, double minX, double minY, double minZ, double maxX, double maxY, double maxZ, float r, float g, float b, float a) {
-        double lineWidth = 0.008;
-        
-        drawLineAsQuad(buffer, matrix, minX, minY, minZ, maxX, minY, minZ, lineWidth, r, g, b, a);
-        drawLineAsQuad(buffer, matrix, maxX, minY, minZ, maxX, minY, maxZ, lineWidth, r, g, b, a);
-        drawLineAsQuad(buffer, matrix, maxX, minY, maxZ, minX, minY, maxZ, lineWidth, r, g, b, a);
-        drawLineAsQuad(buffer, matrix, minX, minY, maxZ, minX, minY, minZ, lineWidth, r, g, b, a);
-        
-        drawLineAsQuad(buffer, matrix, minX, maxY, minZ, maxX, maxY, minZ, lineWidth, r, g, b, a);
-        drawLineAsQuad(buffer, matrix, maxX, maxY, minZ, maxX, maxY, maxZ, lineWidth, r, g, b, a);
-        drawLineAsQuad(buffer, matrix, maxX, maxY, maxZ, minX, maxY, maxZ, lineWidth, r, g, b, a);
-        drawLineAsQuad(buffer, matrix, minX, maxY, maxZ, minX, maxY, minZ, lineWidth, r, g, b, a);
-        
-        drawLineAsQuad(buffer, matrix, minX, minY, minZ, minX, maxY, minZ, lineWidth, r, g, b, a);
-        drawLineAsQuad(buffer, matrix, maxX, minY, minZ, maxX, maxY, minZ, lineWidth, r, g, b, a);
-        drawLineAsQuad(buffer, matrix, maxX, minY, maxZ, maxX, maxY, maxZ, lineWidth, r, g, b, a);
-        drawLineAsQuad(buffer, matrix, minX, minY, maxZ, minX, maxY, maxZ, lineWidth, r, g, b, a);
-    }
-    
-    private static void drawLineAsQuad(BufferBuilder buffer, Matrix4f matrix, double x1, double y1, double z1, double x2, double y2, double z2, double width, float r, float g, float b, float a) {
-        double dx = x2 - x1;
-        double dy = y2 - y1;
-        double dz = z2 - z1;
-        double length = Math.sqrt(dx * dx + dy * dy + dz * dz);
-        
-        if (length == 0) return;
-        
-        dx /= length;
-        dy /= length;
-        dz /= length;
-        
-        double px, py, pz;
-        if (Math.abs(dy) > 0.9) {
-            px = 1; py = 0; pz = 0;
-        } else {
-            px = 0; py = 1; pz = 0;
+        if (outline) {
+            VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getLines());
+            drawOutlineBox(matrix, consumer, box, red, green, blue, alpha);
         }
-        
-        double cx = dy * pz - dz * py;
-        double cy = dz * px - dx * pz;
-        double cz = dx * py - dy * px;
-        double clen = Math.sqrt(cx * cx + cy * cy + cz * cz);
-        cx = (cx / clen) * width;
-        cy = (cy / clen) * width;
-        cz = (cz / clen) * width;
-        
-        buffer.vertex(matrix, (float)(x1 - cx), (float)(y1 - cy), (float)(z1 - cz)).color(r, g, b, a);
-        buffer.vertex(matrix, (float)(x1 + cx), (float)(y1 + cy), (float)(z1 + cz)).color(r, g, b, a);
-        buffer.vertex(matrix, (float)(x2 + cx), (float)(y2 + cy), (float)(z2 + cz)).color(r, g, b, a);
-        buffer.vertex(matrix, (float)(x2 - cx), (float)(y2 - cy), (float)(z2 - cz)).color(r, g, b, a);
     }
 
-    private static void drawFilledBox(Matrix4f matrix, BufferBuilder bufferBuilder, double minX, double minY, double minZ, double maxX, double maxY, double maxZ, float r, float g, float b, float a) {
-        bufferBuilder.vertex(matrix, (float)minX, (float)minY, (float)minZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)maxX, (float)minY, (float)minZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)maxX, (float)minY, (float)maxZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)minX, (float)minY, (float)maxZ).color(r, g, b, a);
+    private static void drawFilledBox(Matrix4f matrix, VertexConsumer consumer, Box box, float r, float g, float b, float a) {
+        float x1 = (float) box.minX;
+        float y1 = (float) box.minY;
+        float z1 = (float) box.minZ;
+        float x2 = (float) box.maxX;
+        float y2 = (float) box.maxY;
+        float z2 = (float) box.maxZ;
 
-        bufferBuilder.vertex(matrix, (float)minX, (float)maxY, (float)minZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)minX, (float)maxY, (float)maxZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)maxX, (float)maxY, (float)maxZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)maxX, (float)maxY, (float)minZ).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y1, z1).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y1, z1).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y1, z2).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y1, z2).color(r, g, b, a);
 
-        bufferBuilder.vertex(matrix, (float)minX, (float)minY, (float)minZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)minX, (float)maxY, (float)minZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)maxX, (float)maxY, (float)minZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)maxX, (float)minY, (float)minZ).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y2, z1).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y2, z2).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y2, z2).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y2, z1).color(r, g, b, a);
 
-        bufferBuilder.vertex(matrix, (float)maxX, (float)minY, (float)minZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)maxX, (float)maxY, (float)minZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)maxX, (float)maxY, (float)maxZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)maxX, (float)minY, (float)maxZ).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y1, z1).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y1, z2).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y2, z2).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y2, z1).color(r, g, b, a);
 
-        bufferBuilder.vertex(matrix, (float)maxX, (float)minY, (float)maxZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)maxX, (float)maxY, (float)maxZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)minX, (float)maxY, (float)maxZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)minX, (float)minY, (float)maxZ).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y1, z1).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y2, z1).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y2, z2).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y1, z2).color(r, g, b, a);
 
-        bufferBuilder.vertex(matrix, (float)minX, (float)minY, (float)maxZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)minX, (float)maxY, (float)maxZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)minX, (float)maxY, (float)minZ).color(r, g, b, a);
-        bufferBuilder.vertex(matrix, (float)minX, (float)minY, (float)minZ).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y1, z1).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y2, z1).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y2, z1).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y1, z1).color(r, g, b, a);
+
+        consumer.vertex(matrix, x1, y1, z2).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y1, z2).color(r, g, b, a);
+        consumer.vertex(matrix, x2, y2, z2).color(r, g, b, a);
+        consumer.vertex(matrix, x1, y2, z2).color(r, g, b, a);
+    }
+
+    private static void drawOutlineBox(Matrix4f matrix, VertexConsumer consumer, Box box, float r, float g, float b, float a) {
+        float x1 = (float) box.minX;
+        float y1 = (float) box.minY;
+        float z1 = (float) box.minZ;
+        float x2 = (float) box.maxX;
+        float y2 = (float) box.maxY;
+        float z2 = (float) box.maxZ;
+
+        line(matrix, consumer, x1, y1, z1, x2, y1, z1, r, g, b, a);
+        line(matrix, consumer, x2, y1, z1, x2, y1, z2, r, g, b, a);
+        line(matrix, consumer, x2, y1, z2, x1, y1, z2, r, g, b, a);
+        line(matrix, consumer, x1, y1, z2, x1, y1, z1, r, g, b, a);
+
+        line(matrix, consumer, x1, y2, z1, x2, y2, z1, r, g, b, a);
+        line(matrix, consumer, x2, y2, z1, x2, y2, z2, r, g, b, a);
+        line(matrix, consumer, x2, y2, z2, x1, y2, z2, r, g, b, a);
+        line(matrix, consumer, x1, y2, z2, x1, y2, z1, r, g, b, a);
+
+        line(matrix, consumer, x1, y1, z1, x1, y2, z1, r, g, b, a);
+        line(matrix, consumer, x2, y1, z1, x2, y2, z1, r, g, b, a);
+        line(matrix, consumer, x1, y1, z2, x1, y2, z2, r, g, b, a);
+        line(matrix, consumer, x2, y1, z2, x2, y2, z2, r, g, b, a);
+    }
+
+    private static void line(Matrix4f matrix, VertexConsumer consumer, float x1, float y1, float z1, float x2, float y2, float z2, float r, float g, float b, float a) {
+        float dx = x2 - x1;
+        float dy = y2 - y1;
+        float dz = z2 - z1;
+        float len = (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
+        if (len > 0) {
+            dx /= len;
+            dy /= len;
+            dz /= len;
+        }
+
+        consumer.vertex(matrix, x1, y1, z1).color(r, g, b, a).normal(dx, dy, dz);
+        consumer.vertex(matrix, x2, y2, z2).color(r, g, b, a).normal(dx, dy, dz);
     }
 }
